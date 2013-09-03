85f5972dd0f3171f5a4f5e84d22ef4aeb98727e4
David P. Sanders <dpsanders@gmail.com>
2013-09-03 09:37:46 -0500

    Agregué test_intervalo.py

---
+++ clases/test_intervalo.py
@@ -0,0 +1,14 @@
+# Escribimos funciones con el nombre test_ALGO
+
+from intervalo import Intervalo
+
+def test_adicion():
+
+	a = Intervalo(1, 2)
+	b = Intervalo(2, 3)
+
+	c = a + b
+
+	# Quiero checar que c, definido asi, esta bien:
+
+	assert c.lo == 3 and c.hi == 5
