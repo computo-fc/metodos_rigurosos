d49e82329b0f73dcba55955ab8814610e1584dbf
Alonso Espinosa <alonso.espinosa@gmail.com>
2013-09-07 20:13:08 -0500

    trabajé más en test

--- julia/tests.jl
+++ julia/tests.jl
@@ -6,7 +6,7 @@ function test_interval()
     assert(Interval(1,3) + Interval(4,5) == Interval(5, 8) )
     assert(Interval(-1, 1) +  Interval(1,3) == Interval(0, 4) )
     assert(Interval(-10, 10) + Interval(-5, 5) == Interval(-15, 15))
-    assert(Interval(-Inf, 10) + Interval(3.0, 2.0) == Interval(-Inf, 12))
+    assert(Interval(-Inf, 10) + Interval(2.38, 3.0) == Interval(-Inf, 13))
     assert(Interval(-Inf, Inf) + Interval(2, 6) == Interval(-Inf, Inf))
     
     #probamos resta
@@ -19,3 +19,5 @@ function test_interval()
     #probamos division
     
 end
+
+test_interval()
