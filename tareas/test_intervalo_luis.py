24c8971a67dd463bc61539d2ecc4af562b2dc4cd
luis <ljml27@gmail.com>
2013-09-14 14:59:33 -0500

    Actualizado relaciones < y > y sus tests

--- tareas/test_intervalo_luis.py
+++ tareas/test_intervalo_luis.py
@@ -14,22 +14,55 @@ def test_adicion():
     assert c.lo == 3 and c.hi == 5
 
 def test_comparacion_lt():
-    '''Se verifica el test < de intervalos'''
-	#Faltan los casos interesantes cuando se 
-	#comparan con un intervalo que tenga 
-	# al cero.
-    a=Intervalo(2,3)
-    b=Intervalo(0,1)
-    c=Intervalo(-1,1)
+    '''Test < de intervalos'''
 
-    assert (a<b) == False and (b<a) == True
-    assert (a<c)
+    a=Intervalo(-1,1)
+    b=Intervalo(0,1)
+    c=Intervalo(1,2)
+    d=Intervalo(2,3)
+
+    assert (a<a) == False
+    assert (a<b) == False and (b<a) == False
+    assert (b<c) == False and (c<b) == False
+    assert (b<d) == True  and (d<b) == False
+    assert (c<d) == False and (d<c) == False
 
 def test_comparacion_gt():
-    '''Se verifica el test > de intervalos'''
-    a=Intervalo(2,3)
+    '''Test > de intervalos'''
+
+    a=Intervalo(-1,1)
     b=Intervalo(0,1)
-    assert (a>b) == True and (b>a) == False
+    c=Intervalo(1,2)
+    d=Intervalo(2,3)
     
+    assert (a>a) == False
+    assert (a>b) == False and (b>a) == False
+    assert (b>c) == False and (c>b) == False
+    assert (b>d) == False and (d>b) == True
+    assert (c>d) == False and (d>c) == False
     
-    
+def test_comparacion_le():
+    '''Test <= de intervalos'''
+    a=Intervalo(-1,1)
+    b=Intervalo(0,1)
+    c=Intervalo(1,2)
+    d=Intervalo(2,3)
+
+    assert (a<=a) == True
+    assert (a<=b) == True and (b<=a) == False
+    assert (b<=c) == True and (c<=b) == False
+    assert (b<=d) == True and (d<=b) == False
+    assert (c<=d) == True and (d<=c) == False
+
+def test_comparacion_ge():
+    '''Test >= de intervalos'''
+    a=Intervalo(-1,1)
+    b=Intervalo(0,1)
+    c=Intervalo(1,2)
+    d=Intervalo(2,3)
+
+    assert (a>=a) == True
+    assert (a>=b) == False and (b>=a) == True
+    assert (b>=c) == False and (c>=b) == True
+    assert (b>=d) == False and (d>=b) == True
+    assert (c>=d) == False and (d>=c) == True
